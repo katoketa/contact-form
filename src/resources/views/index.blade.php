6641f1d513c45b4beec113dadc2c59470c921bab
keita kato <infotec62509@gmail.com>
2025-10-20 22:40:00 +0900

    セットアップの更新中

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+<body>
+    <heder class="header">
+        <div class="header__inner">
+            <a href="/" class="header__logo">
+                Contact Form
+            </a>
+        </div>
+    </heder>
+    <main>
+        
+    </main>
+</body>
+</html>
