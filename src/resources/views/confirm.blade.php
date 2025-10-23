c07cf668c56645e8e60701f310950f45d081e797
keita kato <infotec62509@gmail.com>
2025-10-23 14:38:15 +0900

    マイグレーションによるテーブル作成の実行エラー

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -1,4 +1,3 @@
-<?php print_r($contact['name']) ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -22,7 +21,8 @@
             <div class="confirm__heading">
                 <h2>お問い合わせ内容確認</h2>
             </div>
-            <form action="" class="form">
+            <form action="/contacts" class="form" method="post">
+                @csrf
                 <div class="confirm-table">
                     <table class="confirm-table__inner">
                         <tr class="confirm-table__row">
